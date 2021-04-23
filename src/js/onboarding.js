206d4dcb932e3e84117253347f88c15fff7821e1
anneroemer <77618343+anneroemer@users.noreply.github.com>
2021-04-23 13:06:39 +0200

    added css animation for splashpage

--- src/js/onboarding.js
+++ src/js/onboarding.js
@@ -1,84 +1,90 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const overlay = document.querySelector(".overlay");
-    const nextBtn = overlay.querySelector(".nextBtn");
-    const skipBtn = overlay.querySelector(".skipBtn");
-    const content = overlay.querySelector(".overlay__content");
 
-    //localstModule.create("onboardingComplete", "true")
-    //localStorage.setItem() - ville gøre det samme som koden ovenover
+    if (overlay){ //vi laver dette trin ifbm vores splash page
 
-    function step(message, xcord, ycord){
-        let container = document.createElement("div");
-        container.className = "overlay__tooltip";
+        const nextBtn = overlay.querySelector(".nextBtn");
+        const skipBtn = overlay.querySelector(".skipBtn");
+        const content = overlay.querySelector(".overlay__content");
 
-        let svg = `
-        <svg viewbox="0 0 350 280" width="350" height="280">
-            <circle class="myCircle" cx="60" cy="60" r="40" stroke="yellow" stroke-width="5" fill="none" />
+        //localstModule.create("onboardingComplete", "true")
+        //localStorage.setItem() - ville gøre det samme som koden ovenover
 
-            <path class="tooltip" d="M90,90 
-            h200
-            a40,40 0 0 1 40,40
-            v0
-            a40,40 0 0 1 -40,40
-            h-160 
-            a40,40 0 0 1 -40,-40
-            v-100 z" fill="yellow" />
-        </svg>
-        `;
+        function step(message, xcord, ycord){
+            let container = document.createElement("div");
+            container.className = "overlay__tooltip";
 
-        let tooltipText = document.createElement("p");
-        tooltipText.innerHTML = message;
-        tooltipText.className = "overlay__tooltipText";
+            let svg = `
+            <svg viewbox="0 0 350 280" width="350" height="280">
+                <circle class="myCircle" cx="60" cy="60" r="40" stroke="yellow" stroke-width="5" fill="none" />
 
-        container.innerHTML = svg;
-        container.appendChild(tooltipText);
-        content.appendChild(container);
+                <path class="tooltip" d="M90,90 
+                h200
+                a40,40 0 0 1 40,40
+                v0
+                a40,40 0 0 1 -40,40
+                h-160 
+                a40,40 0 0 1 -40,-40
+                v-100 z" fill="yellow" />
+            </svg>
+            `;
 
-        container.style.top = ycord;
-        container.style.left = xcord;
-    }
+            let tooltipText = document.createElement("p");
+            tooltipText.innerHTML = message;
+            tooltipText.className = "overlay__tooltipText";
 
-    //huske at slette currentStep i localstorage i Browseren, når du tester
-    if (localstModule.read("onboardingComplete") != "true") {
-        step("Click here to view Pokemon details", "60px", "200px")
-        increaseCurrentStep()
-    }else{
-        overlay.remove();
-    }
+            container.innerHTML = svg;
+            container.appendChild(tooltipText);
+            content.appendChild(container);
 
-    nextBtn.addEventListener("click", function(){
-        let currentStep = localstModule.read("currentStep")
-        removeContent();
-
-        if(currentStep == "1"){
-            step("Find Pokemons by their name", "100px", "-8px")
-            increaseCurrentStep()
+            container.style.top = ycord;
+            container.style.left = xcord;
         }
 
-        if(currentStep == "2"){
-            step("Use buttons to switch page", "110px", "420px")
-            increaseCurrentStep()
-        }
+        //huske at slette currentStep i localstorage i Browseren, når du tester
 
-        if(currentStep == "3"){
-            localstModule.create("onboardingComplete", "true");
+        if (localstModule.read("onboardingComplete") != "true") {
+            step("Click here to view Pokemon details", "60px", "200px")
+            increaseCurrentStep()
+        }else{
             overlay.remove();
         }
-    });
 
-    function removeContent(){
-        content.removeChild(content.children[0])
+        nextBtn.addEventListener("click", function(){
+            let currentStep = localstModule.read("currentStep")
+            removeContent();
+
+            if(currentStep == "1"){
+                step("Find Pokemons by their name", "100px", "-8px")
+                increaseCurrentStep()
+            }
+
+            if(currentStep == "2"){
+                step("Use buttons to switch page", "110px", "420px")
+                increaseCurrentStep()
+            }
+
+            if(currentStep == "3"){
+                localstModule.create("onboardingComplete", "true");
+                overlay.remove();
+            }
+        });
+
+        function removeContent(){
+            content.removeChild(content.children[0])
+        }
+
+        function increaseCurrentStep(){
+            let step = localstModule.read("currentStep") ? parseInt(localstModule.read("currentStep")) + 1 : 1;
+            localstModule.create("currentStep", step);
+        }
+
+        skipBtn.addEventListener("click", function(){
+            localstModule.create("onboardingComplete", "true");
+            overlay.remove();
+        })
+
     }
-
-    function increaseCurrentStep(){
-        let step = localstModule.read("currentStep") ? parseInt(localstModule.read("currentStep")) + 1 : 1;
-        localstModule.create("currentStep", step);
-    }
-
-    skipBtn.addEventListener("click", function(){
-        localstModule.create("onboardingComplete", "true");
-        overlay.remove();
-    })
 
 });
